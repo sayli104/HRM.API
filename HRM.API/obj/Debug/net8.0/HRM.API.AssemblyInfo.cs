2c34fb9763491b6de1172910a9bcdbdefa33e293
Sayli Patil <sayli.patil@stspl.com>
2023-10-23 13:24:29 +0530

    Add Index files  after commit

--- HRM.API/obj/Debug/net8.0/HRM.API.AssemblyInfo.cs
+++ HRM.API/obj/Debug/net8.0/HRM.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93acad900b411f8322dbd09e477274ebd826fad5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3945572ad41aeb6f3c73ac357d1c37224f201344")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
