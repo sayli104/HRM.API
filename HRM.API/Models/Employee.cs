90278e8cd83d07a119c76b1ae4d95e1d051d33f8
sayli patil <sayli.patil@STSLP87>
2023-10-27 12:26:50 +0530

    Remove null condition value for Employee Id column

--- HRM.API/Models/Employee.cs
+++ HRM.API/Models/Employee.cs
@@ -2,7 +2,7 @@
 {
     public class Employee
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Designation { get; set; }
         public string? FathersName { get; set; }
--- HRM.API/obj/Debug/net8.0/HRM.API.AssemblyInfo.cs
+++ HRM.API/obj/Debug/net8.0/HRM.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bb94df57db44e70cd3bf7fdcc588c065ca02304")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca7d975b58896b798d07970f1999867a0c3520c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
